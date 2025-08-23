e8a5f32e6e829bd5860956b2de0a5e7a3baa8d3a
itnoyabilisim-commits <itnoyabilisim@gmail.com>
2025-08-24 00:33:41 +0300

    Update index.js

--- services/robot-exec/index.js
+++ services/robot-exec/index.js
@@ -1,1 +1,8 @@
-console.log('robot-exec started'); setInterval(()=>{}, 1<<30)
+// services/robot-exec/index.js
+console.log("robot-exec service started");
+
+// Dummy order consumer
+// (Gerçekte RabbitMQ'dan "orders.v1" subscribe edecek)
+setInterval(() => {
+  console.log("robot-exec heartbeat", new Date().toISOString());
+}, 10000);
