0fe8c6e6ab73846bbe941b5ade26d53c1e964c74
itnoyabilisim-commits <itnoyabilisim@gmail.com>
2025-08-24 14:45:01 +0300

    Create content.js

---
+++ apps/admin/pages/content.js
@@ -0,0 +1,8 @@
+export default function ContentAdmin() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>İçerik Yönetimi (Dummy)</h1>
+      <p>Burada eğitim videoları, SSS ve yenilik duyuruları yönetilecek.</p>
+    </div>
+  );
+}
