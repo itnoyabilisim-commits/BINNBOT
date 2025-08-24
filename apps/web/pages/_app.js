6c5e5d5343c3a33baf0cf2c2a0977629a1a1809f
itnoyabilisim-commits <itnoyabilisim@gmail.com>
2025-08-24 15:05:12 +0300

    Update _app.js

--- apps/web/pages/_app.js
+++ apps/web/pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+        <a href="/dashboard" style={{ marginRight: "15px" }}>Dashboard</a>
+        <a href="/logout" style={{ color: "red" }}>Çıkış</a>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
